c168a0929821ae4649e2f91269e375c898a38e45
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2022-10-26 15:51:25 +0200

    creating routes and serving with warp

--- db/src/main.rs
+++ db/src/main.rs
@@ -11,5 +11,65 @@ async fn main() {
 
     let pet = warp::path!("owner" / i32 / "pet");
     let pet_param = warp::path!("owner" / i32 / "pet" / i32);
-    
+    let owner = warp::path("owner");
+
+    let pet_routes = pet
+        .and(warp::get())
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::list_pets_handler)
+        .or(pet
+            .and(warp::post())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::create_pet_handler)    
+        )
+        .or(
+            pet_param
+            .and(warp::post())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::delete_pet_handler)
+        );
+
+    let owner_routes = owner
+        .and(warp::get())
+        .and(warp::path::param())
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::fetch_owner_handler)
+        .or(owner
+            .and(warp::get())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::list_owner_handler)
+        )
+        .or(owner
+            .and(warp::post())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::create_owner_handler)
+        );
+
+    let routes = pet_routes
+        .or(owner_routes)
+        .recover(error::handle_rejection)
+        .with(
+            warp::cors()
+                .allow_credentials(true)
+                .allow_methods(&[
+                    Method::OPTIONS,
+                    Method::GET,
+                    Method::POST,
+                    Method::DELETE,
+                    Method::PUT,
+                ])
+                .allow_headers(vec![header::CONTENT_TYPE, header::ACCEPT])
+                .expose_headers(vec![header::LINK])
+                .max_age(300)
+                .allow_any_origin(),
+        );
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+}
+
+fn with_db(db_pool: DBPool) -> impl Filter<Exctract = (DBPool,), Error = infallible> + Clone {
+    warp::any().map(move || db_pool.clone())
 }
